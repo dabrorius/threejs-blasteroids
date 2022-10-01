b0a2d663a15cdc45c071c9cad5b2a5fa0fd28cf2
dabrorius <filip.defar@protonmail.com>
2022-10-01 11:13:05 +0200

    feat: Three.js scene boilerplate

--- src/app.js
+++ src/app.js
@@ -1,1 +1,46 @@
-console.log("JS Loaded")
+import * as THREE from "three";
+
+const viewport = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  viewport.width / viewport.height,
+  0.1,
+  100
+);
+camera.position.z = 4;
+scene.add(camera);
+
+const canvas = document.querySelector(".gameCanvas");
+const renderer = new THREE.WebGLRenderer({ canvas });
+const updateRenderer = () => {
+  renderer.setSize(viewport.width, viewport.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+};
+updateRenderer();
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshBasicMaterial({ wireframe: true });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
+window.addEventListener("resize", () => {
+  viewport.width = window.innerWidth;
+  viewport.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = viewport.width / viewport.height;
+  camera.updateProjectionMatrix();
+
+  updateRenderer();
+});
+
+const gameLoop = () => {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(gameLoop);
+};
+gameLoop();
